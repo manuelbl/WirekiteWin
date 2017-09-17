4e863f922ca3f40194f6bb7b0d751f242dcc1146
Manuel Bleichenbacher <manuel.bleichenbacher@gmail.com>
2017-09-17 13:20:43 +0200

    Nunchuck breadboard example

---
+++ Examples/Nunchuck/Nunchuck/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿//
+// Wirekite for Windows 
+// Copyright (c) 2017 Manuel Bleichenbacher
+// Licensed under MIT License
+// https://opensource.org/licenses/MIT
+//
+
+using Codecrete.Wirekite.Device;
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace Nunchuck
+{
+    public partial class MainWindow : Window, IWirekiteDeviceNotification
+    {
+        private WirekiteDevice device;
+        private WirekiteService service;
+
+        private Timer timer;
+        private int i2cPort;
+        private NunchuckController nunchuck;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            service = new WirekiteService(this, this);
+        }
+
+        public void OnDeviceConnected(WirekiteDevice device)
+        {
+            this.device = device;
+            device.ResetConfiguration();
+
+            i2cPort = device.ConfigureI2CMaster(I2CPins.I2CPinsSCL19_SDA18, 100000);
+            nunchuck = new NunchuckController(device, i2cPort);
+            timer = new Timer(ReadNunchuck, null, 0, 100);
+        }
+
+        public void ReadNunchuck(Object stateInfo)
+        {
+            nunchuck.ReadData();
+
+            Dispatcher.Invoke(() =>
+            {
+                joystickXLabel.Content = String.Format("X: {0}", nunchuck.JoystickX);
+                joystickYLabel.Content = String.Format("Y: {0}", nunchuck.JoystickY);
+                acceleroMeterXLabel.Content = String.Format("X: {0}", nunchuck.AccelerometerX);
+                acceleroMeterYLabel.Content = String.Format("Y: {0}", nunchuck.AccelerometerY);
+                acceleroMeterZLabel.Content = String.Format("Z: {0}", nunchuck.AccelerometerZ);
+                buttonCLabel.Content = nunchuck.ButtonC ? "C: Pressed" : "C: -";
+                buttonZLabel.Content = nunchuck.ButtonZ ? "Z: Pressed" : "Z: -";
+            });
+        }
+
+        public void OnDeviceDisconnected(WirekiteDevice device)
+        {
+            timer.Dispose();
+            device = null;
+        }
+    }
+}
---
+++ Examples/Nunchuck/Nunchuck/NunchuckController.cs
@@ -0,0 +1,71 @@
+﻿//
+// Wirekite for Windows 
+// Copyright (c) 2017 Manuel Bleichenbacher
+// Licensed under MIT License
+// https://opensource.org/licenses/MIT
+//
+
+using Codecrete.Wirekite.Device;
+using System;
+
+
+namespace Nunchuck
+{
+    public class NunchuckController
+    {
+        public WirekiteDevice Device;
+        public int I2CPort;
+        public int SlaveAddress = 0x52;
+
+        public int JoystickX;
+        public int JoystickY;
+        public int AccelerometerX;
+        public int AccelerometerY;
+        public int AccelerometerZ;
+        public bool ButtonC;
+        public bool ButtonZ;
+
+        public NunchuckController(WirekiteDevice device, int i2cPort)
+        {
+            Device = device;
+            I2CPort = i2cPort;
+            InitController();
+        }
+
+        public void ReadData()
+        {
+            // Read the six bytes with the sensor data
+            byte[] sensorData = Device.RequestDataOnI2CPort(I2CPort, SlaveAddress, 6);
+            if (sensorData == null || sensorData.Length != 6)
+                throw new Exception(String.Format("Nunchuck read failed - reason {0}", Device.GetLastI2CResult(I2CPort)));
+
+            // Assign the sensor data to the public variables
+            JoystickX = sensorData[0];
+            JoystickY = sensorData[1];
+            AccelerometerX = (sensorData[2] << 2) | ((sensorData[5] >> 2) & 0x3);
+            AccelerometerY = (sensorData[3] << 2) | ((sensorData[5] >> 4) & 0x3);
+            AccelerometerZ = (sensorData[4] << 2) | ((sensorData[5] >> 6) & 0x3);
+            ButtonC = (sensorData[5] & 2) == 0;
+            ButtonZ = (sensorData[5] & 1) == 0;
+
+            // Prepare the next data read (start conversion)
+            Device.SubmitOnI2CPort(I2CPort, new byte[] { 0 }, SlaveAddress);
+        }
+
+
+        private void InitController()
+        {
+            byte[][] initSequences = new byte[][] {
+                new byte[] { 0xf0, 0x55 },
+                new byte[] { 0xfb, 0x00 }
+            };
+        
+            foreach (byte[] seq in initSequences)
+            {
+                int numBytes = Device.SendOnI2CPort(I2CPort, seq, SlaveAddress);
+                if (numBytes != seq.Length)
+                    throw new Exception(String.Format("Nunchuck init failed - reason {0}", Device.GetLastI2CResult(I2CPort)));
+            }
+        }
+    }
+}
